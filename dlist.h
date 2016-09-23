999b73e023977f6d56cf8b8189f6a1c3d899166b
cathyatseneca <cathyatseneca@gmail.com>
2016-09-23 16:06:33 -0400

    lists

--- dlist.h
+++ dlist.h
@@ -8,28 +8,56 @@ class DList{
 		Node* next_;
 		Node* prev_;
 		Node(const T& data,Node* next=nullptr,Node* prev=nullptr){
-
+			data_=data;
+			next_=next;
+			prev_=prev;
 		}
 	};
 	Node* head_;
 	Node* tail_;
 public:
 	DList(){
+		head_=tail_=nullptr;
 	}
 	void push_front(const T& data){
+		Node* temp=new Node(data,head_);
+		if(head_){
+			head_->prev_=temp;
+		}
+		else{
+			tail_=temp;
+		}
+		head_=temp;
 
 	}
 	void push_back(const T& data){
 
 	}
 	void pop_front(){
-		
+
 	}
 	void pop_back(){
+		if(head_){
+			if(head_!=tail_){
+				Node* last=tail_;
+				Node* secondLast=tail_->prev_;
+				secondLast->next_=nullptr;
+				delete last;
+				tail_=secondLast;
+			}
+			else{
+				delete head_;
+				head_=tail_=nullptr;
+			}
+		}
 
 	}
 	void print() const{
-
+		Node* it=head_;
+		while(it){
+			cout << it->data_ << endl;
+			it=it->next_;
+		}
 	}
 };
 
--- slist.h
+++ slist.h
@@ -7,15 +7,23 @@ class SList{
 		T data_;
 		Node* next_;
 		Node(const T& data,Node* next=nullptr){
-
+			data_=data;
+			next_=next;
 		}
 	};
 	Node* head_;
 	Node* tail_;
 public:
 	SList(){
+		head_=tail_=nullptr;
 	}
 	void push_front(const T& data){
+		Node* temp=new Node(data,head_);
+		if(!head_){
+			tail_=temp;
+		}
+		head_=temp;
+
 
 	}
 	void push_back(const T& data){
@@ -25,9 +33,29 @@ public:
 		
 	}
 	void pop_back(){
-
+		if(head_){
+			if(head_!=tail_){
+				Node* last=tail_;
+				Node* secondLast=head_;//=tail_->prev_;
+				while(secondLast->next_!=tail_){
+					secondLast=secondLast->next_;
+				}
+				secondLast->next_=nullptr;
+				delete last;
+				tail_=secondLast;
+			}
+			else{
+				delete head_;
+				head_=tail_=nullptr;
+			}
+		}	
 	}
 	void print() const{
+		Node* it=head_;
+		while(it){
+			cout << it->data_ << endl;
+			it=it->next_;
+		}
 
 	}
 };
