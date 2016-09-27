0b54f051c67bfd60a52612311321dcc8ec5b865c
cathyatseneca <cathyatseneca@gmail.com>
2016-09-27 16:44:39 -0400

    sentinels

---
+++ sentinels.h
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class DList{
+	struct Node{
+		T data_;
+		Node* next_;
+		Node* prev_;
+		Node(const T& data = T{},Node* next=nullptr,Node* prev=nullptr){
+			data_=data;
+			next_=next;
+			prev_=prev;
+		}
+	};
+	Node* head_;
+	Node* tail_;
+public:
+	DList(){
+		head_=new Node();  //create front sentinel
+		tail_=new Node();  //create back sentinel
+		head_->next_=tail_;
+		tail_->prev_=head_;
+
+	}
+	void push_front(const T& data){
+		//this function adds a node between 
+		//front sentinel and the first node in list
+
+		Node* first=head_->next_; //it is ok if this
+		                          //is back sentinel
+		Node* temp=new Node(data,first,head_);
+		head_->next_=temp;
+		first->prev_=temp;
+
+	}
+	void push_back(const T& data){
+
+	}
+	void pop_front(){
+
+	}
+	void pop_back(){
+		Node* last=tail_->prev_;
+		//checks if list is empty
+		if(last!=head_){
+			Node* secondLast=last->prev_;
+			tail_->prev_=secondLast;
+			secondLast->next_=tail_;
+			delete last;
+		}
+
+	}
+	void print() const{
+		Node* it=head_->next_;
+		while(it!=tail_){
+			cout << it->data_ << endl;
+			it=it->next_;
+		}
+	}
+};
+
